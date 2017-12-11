137c8d11868691ea43709f16774ca64022b23a3b
Alejandro <cesar.leo14@yahoo.com>
2017-12-11 11:22:12 -0600

    Initial commit

---
+++ Assets/Scripts/ButtonCredits.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonCredits : MonoBehaviour
+{
+    public void CreditsButtonClicked()
+    {
+        SceneManager.LoadScene("Credits Scene");
+    }
+}
---
+++ Assets/Scripts/ButtonStartGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonStartGame : MonoBehaviour
+{
+    public void StartGameButtonClicked()
+    {
+        SceneManager.LoadScene("World Scene");
+    }
+}
